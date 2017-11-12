da4fa7488dfb596c20fbcf15fae72edcb169a343
jianglin <1262298614@qq.com>
2017-11-13 01:44:05 +0800

    RecyclerView的封装（二）

--- ITalkerProject2/common/src/main/java/italker/tencent/com/common/weiget/recycler/RecyclerAdapter.java
+++ ITalkerProject2/common/src/main/java/italker/tencent/com/common/weiget/recycler/RecyclerAdapter.java
@@ -1,10 +1,13 @@
 package italker.tencent.com.common.weiget.recycler;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,11 +16,38 @@ import java.util.List;
 
 public abstract class RecyclerAdapter<Data> extends RecyclerView.Adapter<MyViewHolder<Data>> implements View.OnClickListener,View.OnLongClickListener,RecyclerCallBack{
 
+    protected Context context;
     protected List<Data> mList = new ArrayList<Data>();
+    protected  AdapterListener mListener;
+
+    public RecyclerAdapter(List<Data> mList, Context context){
+        this.mList = mList;
+        this.context = context;
+    }
+    /**
+     * 复写默认的布局类型返回
+     *
+     * @param position 坐标
+     * @return 类型，其实复写后返回的都是XML文件的ID
+     */
+    @Override
+    public int getItemViewType(int position) {
+        return getItemViewType(position, mList.get(position));
+    }
+
+    /**
+     * 得到布局的类型
+     *
+     * @param position 坐标
+     * @param data     当前的数据
+     * @return XML文件的ID，用于创建ViewHolder
+     */
+    protected abstract int getItemViewType(int position, Data data);
+
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View mRoot = View.inflate(null,viewType,null);
+        View mRoot = View.inflate(context,viewType,null);
         MyViewHolder<Data> mHolder = getViewHolder(mRoot,viewType);
         mRoot.setOnClickListener(this);
         mRoot.setOnLongClickListener(this);
@@ -34,12 +64,107 @@ public abstract class RecyclerAdapter<Data> extends RecyclerView.Adapter<MyViewH
         Data data = mList.get(position);
         //触发绑定
         holder.onbind();
+
     }
 
     @Override
     public int getItemCount() {
         return mList.size();
     }
+
+    /**
+     * 添加了一条数据
+     * @param data
+     */
+    public void addData(Data data){
+        mList.add(data);
+        notifyItemInserted(mList.size() - 1);
+    }
+    /**
+     * 添加了一堆数据
+     */
+    public void addData(Data...data){
+        if(data.length > 0 && data != null){
+            int start = data.length;
+            Collections.addAll(mList,data);
+            notifyItemRangeChanged(start,mList.size());
+        }
+    }
+    /**
+     * 删除操作
+     */
+    public void clear() {
+        mList.clear();
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 添加一堆数据
+     */
+    public void addData(Collection<Data> data){
+        mList.addAll(data);
+        notifyDataSetChanged();
+    }
+    /**
+     * 替换为一个新的集合，其中包括了清空
+     *
+     * @param dataList 一个新的集合
+     */
+    public void replace(Collection<Data> dataList) {
+        mList.clear();
+        if (dataList == null || dataList.size() == 0)
+            return;
+        mList.addAll(dataList);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+        MyViewHolder viewHolder = (MyViewHolder) v.getTag();
+        if (this.mListener != null) {
+            // 得到ViewHolder当前对应的适配器中的坐标
+            int pos = viewHolder.getAdapterPosition();
+            // 回掉方法
+            this.mListener.onItemClick(viewHolder, mList.get(pos));
+        }
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        MyViewHolder viewHolder = (MyViewHolder) v.getTag();
+        if (this.mListener != null) {
+            // 得到ViewHolder当前对应的适配器中的坐标
+            int pos = viewHolder.getAdapterPosition();
+            // 回掉方法
+            this.mListener.onItemLongClick(viewHolder, mList.get(pos));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 设置适配器的监听
+     *
+     * @param adapterListener AdapterListener
+     */
+    public void setListener(AdapterListener<Data> adapterListener) {
+        this.mListener = adapterListener;
+    }
+
+    /**
+     * 我们的自定义监听器
+     *
+     * @param <Data> 范型
+     */
+    public interface AdapterListener<Data> {
+        // 当Cell点击的时候触发
+        void onItemClick(MyViewHolder holder, Data data);
+
+        // 当Cell长按时触发
+        void onItemLongClick(MyViewHolder holder, Data data);
+    }
+
 }
 abstract class MyViewHolder<Data> extends RecyclerView.ViewHolder{
     protected Data mData;
